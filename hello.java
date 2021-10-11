bacce1f4f39286a921eb6941df4934e75b9c0a9c
karankotak <karankotak94@gmail.com>
2021-10-11 20:33:08 +0530

    Branch Merge
    Merged Developer to master

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ public class hello{
         public static void main(String[] args){
                 for(int i=0;i<=10;i++){
                 
-                System.out.println("Working Making merge changes for master "+i);
+                System.out.println("Working Making merge changes for master hello "+i);
 		}
         }
 }
