146cf8937df33992e177e1c9ed9a7ba4efc551c6
karankotak <karankotak94@gmail.com>
2021-10-11 20:15:56 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ public class hello{
         public static void main(String[] args){
                 for(int i=0;i<=10;i++){
                 
-                System.out.println("Work done final 123 last"+i);
+                System.out.println("Working on dev"+i);
 		}
         }
 }
