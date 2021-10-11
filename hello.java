a3b8fcea9fbd34386fca96a738f329eeba5281cc
karankotak <karankotak94@gmail.com>
2021-10-11 20:30:25 +0530

    done

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ public class hello{
         public static void main(String[] args){
                 for(int i=0;i<=10;i++){
                 
-                System.out.println("Working on devloped "+i);
+                System.out.println("Working Making merge changes for master "+i);
 		}
         }
 }
