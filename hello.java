bf29b3b6673b783ba73398d86829928c3bba187e
karankotak <karankotak94@gmail.com>
2021-10-12 11:23:29 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ public class hello{
         public static void main(String[] args){
                 for(int i=0;i<=10;i++){
                 
-                System.out.println("Working changes on for making pull request check "+i);
+                System.out.println("check "+i);
 		}
         }
 }
